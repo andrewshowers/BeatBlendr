3232fc9ab0fdad251f987711f3f7694de6e6b6a5
Andrew <andrewjshowers@gmail.com>
2023-12-09 01:03:21 -0500

    got the dropdown to work for genre field in the InsertPage. It's not the prettiest, but it works as intended.

--- app/lib/insert_data_page.dart
+++ app/lib/insert_data_page.dart
@@ -10,6 +10,21 @@ class _InsertionPageState extends State<InsertionPage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController artistController = TextEditingController();
   TextEditingController genreController = TextEditingController();
+  List<String> genre_options = [
+    'Classical',
+    'Pop',
+    'Jazz',
+    'Hip Hop',
+    'Country'
+  ];
+  String? selectedItem;
+
+  // I was having a lot of trouble trying to make the dropdown default to null, but this fixed it
+  @override
+  void initState() {
+    super.initState(); // this line is necessary, but I am slightly confused as to why.  Does not work without though.
+    selectedItem = null; // Set selectedItem to null
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,27 +45,67 @@ class _InsertionPageState extends State<InsertionPage> {
               controller: artistController,
               decoration: InputDecoration(labelText: 'Artist'),
             ),
-            TextField(
-              controller: genreController,
+            DropdownButtonFormField<String>(
               decoration: InputDecoration(labelText: 'Genre'),
+              value: selectedItem,
+              items: [
+                DropdownMenuItem<String>(
+                  child: Text(''), // Blank item
+                  value: null,
+                ),
+                ...genre_options.map(
+                  (item) => DropdownMenuItem<String>(
+                    child: Text(item, style: TextStyle(fontSize: 20)),
+                    value: item,
+                  ),
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedItem = value;
+                });
+              },
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                // Create a new Song object from user input
-                Song newSong = Song(
-                  title: titleController.text,
-                  artist: artistController.text,
-                  genre: genreController.text,
-                );
+                if (selectedItem != null) {
+                  // Create a new Song object from user input
+                  Song newSong = Song(
+                    title: titleController.text,
+                    artist: artistController.text,
+                    genre: selectedItem ??
+                        'DefaultGenre',
+                  );
 
-                // Add the new song to the 'songs' list using the helper class
-                Song.addSong(newSong);
+                  // Add the new song to the songs list
+                  Song.addSong(newSong);
 
-                // Optionally, you can clear the text controllers
-                titleController.clear();
-                artistController.clear();
-                genreController.clear();
+                  // clears the text controllers
+                  titleController.clear();
+                  artistController.clear();
+                  setState(() {
+                    selectedItem = null; // Reset the dropdown to null after submitting
+                  });
+                } else { // if genre isn't selected, shows an error box
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Error'),
+                        content: Text('Please select a genre.'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
               },
               child: Text('Insert Song'),
             ),
