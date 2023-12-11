15f86c47dcc7a22aceef425fa73df91372003b55
andrewshowers <andrewjshowers@gmail.com>
2023-12-10 20:51:36 -0800

    working on the landing page. Not complete, but I need to push this before leaving the library

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -4,92 +4,63 @@ import 'package:flutter/material.dart';
 import 'display_data_page.dart';
 import 'insert_data_page.dart';
 import 'genre_buttons_page.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 108, 108, 108), // Grey background
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1), // white background
       appBar: AppBar(
-        title: Text(
-          'BEAT BLENDR',
-          style: TextStyle(
-            color: Colors.orange, // Title text in blue
-            fontSize: 60,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black87),
+          onPressed: () {},
+        ),
       ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GenreButtonsPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.orange, // Set the button color to orange
-                padding: EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 50), // Larger size
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
               ),
-              child: Text(
-                'Give me a song recomendation',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DisplayDataPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.orange, // Set the button color to orange
-                padding: EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 50), // Larger size
-              ),
-              child: Text(
-                'Song library',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InsertionPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.orange, // Set the button color to orange
-                padding: EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 50), // Larger size
-              ),
-              child: Text(
-                'Add a song to the library',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Find Your', style: TextStyle(color: Colors.black87, fontSize: 25),),
+                  SizedBox(height: 3),
+                  // Text(
+                  //   'Inspiration',
+                  //   style: TextStyle(
+                  //     color: Colors.black87,
+                  //     fontSize: 40,
+                  //   ),
+                  // ),
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Hello world!',
+                        textStyle: const TextStyle(
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      speed: const Duration(milliseconds: 2000),
+                      ),
+                    ],
+
+                    totalRepeatCount: 4,
+                    pause: const Duration(milliseconds: 1000),
+                    displayFullTextOnTap: true,
+                    stopPauseSOnTap: true,
+                ) 
+                ],
               ),
             ),
           ],
