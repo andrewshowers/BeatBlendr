a3e023b21dacd51310e70c0869817fdac5cff00d
andrewshowers <andrewjshowers@gmail.com>
2023-12-11 12:48:48 -0800

    brendan's code. Having issues pushing

--- lib/genre_buttons_page.dart
+++ lib/genre_buttons_page.dart
@@ -2,134 +2,184 @@ import 'package:flutter/material.dart';
 import 'song.dart';
 
 class GenreButtonsPage extends StatelessWidget {
+  // Map of genres to image paths
+  final Map<String, String> genreToImage = {
+    'Hip-Hop': 'assets/images/Hip-Hop.jpg',
+    'Pop': 'assets/images/Pop.jpg',
+    'Jazz': 'assets/images/Jazz.jpg',
+    'Classical': 'assets/images/Classical.jpg',
+    'Country': 'assets/images/Country.jpg',
+    // Add more genres and their corresponding images if needed
+  };
+
   @override
   Widget build(BuildContext context) {
+    // List of genres
+    List<String> genres = ['Hip-Hop', 'Pop', 'Jazz', 'Classical', 'Country'];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Genre Selection Page'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // This is the easiest way to make the buttons "modular" so to speak. As opposed to creating a separate raised button for each, there is essentially a class called genrebutton that can be reused.
-          buildGenreButton('Hip-Hop', context),
-          buildGenreButton('Pop', context),
-          buildGenreButton('Jazz', context),
-          buildGenreButton('Classical', context),
-          buildGenreButton('Country', context),
-          buildRandomButton(context),
-        ],
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Number of columns
+          childAspectRatio: 1.0, // Aspect ratio for each item
+          crossAxisSpacing: 10, // Horizontal space between items
+          mainAxisSpacing: 10, // Vertical space between items
+        ),
+        itemCount: genres.length + 1,
+        itemBuilder: (context, index) {
+          if (index < genres.length) {
+            String genre = genres[index];
+            String imagePath = genreToImage[genre] ?? 'default_image_path.jpg';
+            return buildGenreButton(genre, imagePath, context);
+          } else {
+            return buildRandomButton(context);
+          }
+        },
       ),
     );
   }
 
-  Widget buildGenreButton(String genre, BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // Button color
-            onPrimary: Colors.white, // Text color
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          ),
-          // When the button is presses, we create a new list, convert the button genre and list genre to lowecase to make sure they're the same, and fill the list all the songs of that genre
-          onPressed: () {
-            Song? randomSong = Song.randomSongByGenre(Song.songs, genre);
-            if (randomSong != null) {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Random Song - $genre'),
-                    content: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Title: ${randomSong.title}'),
-                        Text('Artist: ${randomSong.artist}'),
-                        Text('Genre: ${randomSong.genre}'),
-                      ],
-                    ),
-                    actions: [
-                      // used text button instead of raised button because it looks nicer and is easier to implement
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('Close'),
-                      ),
+  Widget buildGenreButton(
+      String genre, String imagePath, BuildContext context) {
+    // Define the size of the square
+    double size = MediaQuery.of(context).size.width / 2 -
+        20; // Adjust the divisor to fit the layout
+
+    return Container(
+      width: size,
+      height: size,
+      margin: const EdgeInsets.all(10), // spacing between buttons
+      child: ElevatedButton(
+        // When the button is presses, we create a new list, convert the button genre and list genre to lowecase to make sure they're the same, and fill the list all the songs of that genre
+        onPressed: () {
+          Song? randomSong = Song.randomSongByGenre(Song.songs, genre);
+          if (randomSong != null) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Random Song - $genre'),
+                  content: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Title: ${randomSong.title}'),
+                      Text('Artist: ${randomSong.artist}'),
+                      Text('Genre: ${randomSong.genre}'),
                     ],
-                  );
-                },
-              );
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('No songs available for $genre'),
-                ),
-              );
-            }
-          },
-          child: Text(
-            genre,
-            style: TextStyle(fontSize: 18),
+                  ),
+                  actions: [
+                    // used text button instead of raised button because it looks nicer and is easier to implement
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Close'),
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('No songs available for $genre'),
+              ),
+            );
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.zero,
+          backgroundColor: Colors.transparent,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+            side: BorderSide(color: Colors.black, width: 4), // button border
           ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage(imagePath),
+              width: size,
+              height: size,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 10,
+              child: Text(
+                // Text under each button
+                genre,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-  
+
   buildRandomButton(BuildContext context) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // Button color
-            onPrimary: Colors.white, // Text color
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          ),
           // When the button is presses, we create a new list, convert the button genre and list genre to lowecase to make sure they're the same, and fill the list all the songs of that genre
           onPressed: () {
             Song randomSong = Song.randomSong(Song.songs);
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Random Song'),
-                    content: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Title: ${randomSong.title}'),
-                        Text('Artist: ${randomSong.artist}'),
-                        Text('Genre: ${randomSong.genre}'),
-                      ],
-                    ),
-                    actions: [
-                      // used text button instead of raised button because it looks nicer and is easier to implement
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Close'),
-                      ),
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Random Song'),
+                  content: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Title: ${randomSong.title}'),
+                      Text('Artist: ${randomSong.artist}'),
+                      Text('Genre: ${randomSong.genre}'),
                     ],
-                  );
-                },
-              );
-            }, child: const Text(
-              'I\'m Feeling Lucky',
-              style: TextStyle(fontSize: 18),
-            ),
-        )));
-}
+                  ),
+                  actions: [
+                    // used text button instead of raised button because it looks nicer and is easier to implement
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.purple, // Set your desired color here
+            foregroundColor: Colors.white, // This is the text color
+            // Additional styling can be added here if needed
+          ),
+          child: const Text(
+            'I\'m Feeling Lucky!',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
 
-void main() {
-  runApp(MaterialApp(
-    home: GenreButtonsPage(),
-  ));
-}
+  void main() {
+    runApp(MaterialApp(
+      home: GenreButtonsPage(),
+    ));
+  }
 }
