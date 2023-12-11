1e056ffbbffd2c56b4ea34401f7de9e8035ffa53
Andrew <andrewjshowers@gmail.com>
2023-12-11 03:22:57 -0500

    revamped the landing page. UI looks a lot better.

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -26,7 +26,7 @@ class LandingPage extends StatelessWidget {
       ),
       drawer: Drawer(
         child: ListView(
-          padding: EdgeInsets.zero,
+          padding: EdgeInsets.only(top: 25),
           children: [
             ListTile(
               title: Text('Home'),
@@ -103,14 +103,149 @@ class LandingPage extends StatelessWidget {
                       TyperAnimatedText('Artist', textStyle: const TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold,), speed: const Duration(milliseconds: 100),),
                       TyperAnimatedText('Beat', textStyle: const TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold,), speed: const Duration(milliseconds: 100),),
                     ],
-                    totalRepeatCount: 4,
+                    // totalRepeatCount: 4,
+                    repeatForever: true,
                     displayFullTextOnTap: true,
-                    stopPauseOnTap: true,
+                    // stopPauseOnTap: true,
                   ),
-                ],
+                  SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(244, 243, 243, 1),
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black87), // Add a border
+                        image: DecorationImage(
+                          image: AssetImage('images/music.jpg'),
+                          fit: BoxFit.cover,
+                        )
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => GenreButtonsPage(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.audiotrack,
+                              color: Colors.black87,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Let's Go!",
+                              style: TextStyle(color: Colors.black87, fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Featured Artists',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          promoCard('images/21savage.jpg'),
+                          promoCard('images/taylorswift.jpg'),
+                          promoCard('images/future.jpg'),
+                          promoCard('images/kanye.jpg'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('images/spotify.jpg')),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [
+                                0.3,
+                                0.9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.2)
+                              ]),
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              'Explore on Spotify',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+  }
+
+  Widget promoCard(image) {
+    return AspectRatio(
+      aspectRatio: 2.62 / 3,
+      child: Container(
+        margin: EdgeInsets.only(right: 15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+                0.1,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.1)
+              ])),
         ),
       ),
     );
